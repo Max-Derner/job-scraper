07caaebc9fb5f937f6c58a26a70e71e3ba14864a
Max Derner <max.derner@BJSS.com>
2023-04-29 17:06:10 +0100

    this stuff does all the file io stuff, surprising init?

---
+++ file_io.py
@@ -0,0 +1,86 @@
+from types import TracebackType
+from typing import Type, Dict, Union
+import json
+from common import Directories, format_exception
+from futils import ensure_directories_present
+
+
+PASSWORD_FILE = "passwords.json"
+EMAIL_ADDRESSES_FILE = "email_addresses.json"
+
+
+def write_content(text: str, source: str):
+    directory = Directories.DEBUG
+    ensure_directories_present(directories=[directory])
+    with open(f"{directory}/{source}.txt", mode='w') as io_wrapper:
+        io_wrapper.write(text)
+
+
+def write_exception_report(
+        dest_directory: str,
+        web_page_alias: str,
+        exception_type: Type[BaseException],
+        exception_value: BaseException,
+        exception_traceback: TracebackType
+        ):
+
+    formatted_trace_back = format_exception(
+        exception_type=exception_type,
+        exception_value=exception_value,
+        exception_traceback=exception_traceback
+    )
+
+    ensure_directories_present(directories=[dest_directory])
+    exception_report_file_path = f"{dest_directory}/{web_page_alias}.txt"
+    with open(exception_report_file_path, mode='w') as io_wrapper:
+        report = ''
+        report += '-~- EXCEPTION REPORT -~-'
+        report += '\n'
+        report += web_page_alias
+        report += '\n'
+        report += formatted_trace_back
+        report += '\n\n\n'
+        io_wrapper.write(report)
+
+
+def get_arbitrary_password(password_key: str) -> Union[str, None]:
+    return _fetch_key_from_json_file(
+        json_file=PASSWORD_FILE,
+        key=password_key
+        )
+
+
+def get_email_address(email_alias: str) -> str:
+    email_addr = _fetch_key_from_json_file(
+        json_file=EMAIL_ADDRESSES_FILE,
+        key=email_alias
+        )
+    if email_addr is None:
+        raise KeyError(f"Key: '{email_alias}' returned no associated email address")  # noqa: E501
+    else:
+        return email_addr
+
+
+def _fetch_key_from_json_file(json_file: str, key: str) -> Union[str, None]:
+    with open(json_file, mode='r') as f:
+        json_file_as_dict: Dict[str, str] = json.load(f)
+    return json_file_as_dict.get(key)
+
+
+def get_app_password() -> str:
+    pass_key = "APP_PASSWORD"
+    password = get_arbitrary_password(password_key=pass_key)
+    if password is None:
+        raise KeyError(f"The key '{pass_key}' is not valid for the password json file.")  # noqa: E501
+    else:
+        return password
+
+
+def _fetch_whole_json_object(json_file: str) -> Dict:
+    with open(json_file, mode='r') as f:
+        json_file_as_dict = json.load(f)
+    return json_file_as_dict
+
+
+def get_sites_dict() -> Dict:
+    return _fetch_whole_json_object(json_file="sites.json")
