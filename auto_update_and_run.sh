9d7b8e163ff372d0a79115e04b4cd41e134eae25
Max Derner <max.derner@BJSS.com>
2023-05-08 15:11:52 +0100

    more fixes for cronjob

--- auto_update_and_run.sh
+++ auto_update_and_run.sh
@@ -9,6 +9,9 @@ source jobsworth/bin/activate
 echo "Ensuring requirements installed"
 pip install -r requirements.txt
 
+echo "Setting DISPLAY"
+export DISPLAY=":0"  # also messes up cronjob if removed
+
 echo -e "\nStarting up job"
 echo -e "\n-~- -~- -~- -~- -~- -~- -~- -~- -~- -~-\n"
 python3 main.py --headless-operation  # headless operation must be set for cronjob to execute
